63cb667dd25614d1aab36dbcea6a89b9827ddeb2
Cristian <cristian21rv15@outlook.com>
2019-10-29 00:02:12 -0600

    nuevo 1.0

---
+++ D4_for.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class D4_for here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class D4_for
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class D4_for
+     */
+    public D4_for()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
